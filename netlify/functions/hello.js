278f17ff316cfc2b4582d5340746fa1f720510ed
TacoTuesday365 <soehling@gmail.com>
2025-03-05 14:47:26 -0500

    Delete netlify/functions/hello.js

--- netlify/functions/hello.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "PromptArchi helps you design effective AI prompts using structured frameworks, guiding you to unlock the full potential of language models.",
-    }),
-  };
-};
