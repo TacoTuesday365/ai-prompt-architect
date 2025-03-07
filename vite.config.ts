cfe95852bb532b28e726169d6045f5d85cf773ea
TacoTuesday365 <soehling@gmail.com>
2025-03-07 14:52:17 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   }
 }) 
